9aef60f387d6703ff24d70759875211855e298cf
adityarevanth <adityarevanth@lyrostech.com>
2024-03-13 17:22:34 +0530

    name

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,7 +3,7 @@ import React from "react";
 const Title = () => {
   return (
     <div className="title">
-      <h2>todo list srikanth</h2>
+      <h2>Aditya</h2>
       <div className="underline"></div>
     </div>
   );
